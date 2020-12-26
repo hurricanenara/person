a902d0798770ef216aeac678ecebd69fe06ed25a
naracodes <naralee0124@gmail.com>
2020-12-26 11:52:13 -0500

    finalize PersonController for GET, POST, PUT, DELETE methods

--- src/main/java/com/example/person/controller/PersonController.java
+++ src/main/java/com/example/person/controller/PersonController.java
@@ -2,10 +2,10 @@ package com.example.person.controller;
 
 import com.example.person.domain.Person;
 import com.example.person.domain.PersonRepository;
+import com.example.person.domain.PersonRequestDto;
 import com.example.person.service.PersonService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,20 @@ public class PersonController {
     @GetMapping("/api/persons")
     public List<Person> getPersons() { return personRepository.findAll(); }
 
-    
+    @PostMapping("/api/persons")
+    public Person createPerson(@RequestBody PersonRequestDto requestDto) {
+        Person newPerson = new Person(requestDto);
+        return personRepository.save(newPerson);
+    }
+
+    @PutMapping("/api/persons/{id}")
+    public Long updatePerson(@PathVariable Long id, @RequestBody PersonRequestDto requestDto) {
+        return personService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/persons/{id}")
+    public Long deletePerson(@PathVariable Long id) {
+        personRepository.deleteById(id);
+        return id;
+    }
 }
